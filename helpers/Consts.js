00e217852e87de11bec42c995589272d47f3212a
Charan Madhu <charanmadhu@yandex.com>
2024-06-17 00:28:10 +0530

    Adding infura URL

--- helpers/Consts.js
+++ helpers/Consts.js
@@ -1,2 +1,5 @@
-export const sepoliaRpcUrl = "https://1rpc.io/sepolia";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const sepoliaRpcUrl = `https://sepolia.infura.io/v3/${process.env.INFURA_API}`;
 export const whitelistContractAddress = "0x249Ff81042619747E259A4E8eF6bf6921bB511C0"
